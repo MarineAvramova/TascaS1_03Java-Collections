201353c54c5f4fdbc95ecf4a795f305fc8bd9ea8
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-02-14 13:09:52 +0100

    Update n1Ejercicio3.java
    Modified the absolute path

--- n1Ejercicio3.java
+++ n1Ejercicio3.java
@@ -3,7 +3,20 @@ public class n1Ejercicio3 {
 
 	public static void main(String[] args) {
 		
-		final  String filePath = "C:\\Users\\Marine\\Desktop\\Specialization\\countries.txt";
+		final  String filePath = "countries.txt";
+		try {
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
 		 CountriesHash countriesHash = new CountriesHash(filePath);
 		    Interaction interaction = new Interaction(countriesHash);
 		    interaction.startGame();
